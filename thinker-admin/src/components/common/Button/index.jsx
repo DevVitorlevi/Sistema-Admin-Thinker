7bd6082444b5706b56712a980b7f394d6bda1cb0
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-12 13:03:23 -0300

    Feat: Feito o Index.jsx

---
+++ thinker-admin/src/components/common/Button/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as S from './styles';
+
+const Button = ({ children, variant, size, fullWidth, ...props }) => {
+    return (
+        <S.ButtonWrapper
+            variant={variant}
+            size={size}
+            fullWidth={fullWidth}
+            {...props}
+        >
+            {children}
+        </S.ButtonWrapper>
+    );
+};
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    variant: PropTypes.oneOf(['primary', 'secondary', 'danger', 'warning']),
+    size: PropTypes.oneOf(['sm', 'md', 'lg']),
+    fullWidth: PropTypes.bool,
+};
+
+Button.defaultProps = {
+    variant: 'primary',
+    size: 'md',
+    fullWidth: false,
+};
+
+export default Button;
